c64fa99a925d54b7f7d051d04f096694f00aa0cb
Frederico Setra <frederico_setra@hotmail.com>
2022-12-22 16:39:44 -0300

    feat: header WIP

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,26 @@
 import * as S from "./styles";
 
 function Header() {
-  return <S.Container>teste</S.Container>;
+  return (
+    <S.Container>
+      <S.AlignLeftItens>
+        <S.LogoCustom />
+        <S.Title>Poke Project</S.Title>
+        <S.Description>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </S.Description>
+        <S.ButtonCustom>Fale Comigo</S.ButtonCustom>
+      </S.AlignLeftItens>
+      <S.AlignRightItens>
+        <S.SquareWallpaper>
+          <S.CharizardImg />
+        </S.SquareWallpaper>
+      </S.AlignRightItens>
+    </S.Container>
+  );
 }
 
 export default Header;
--- src/components/header/styles.jsx
+++ src/components/header/styles.jsx
@@ -1,6 +1,101 @@
 import styled from "styled-components";
+import Logo from "../../assets/img/logo1.png";
+import Charizard from "../../assets/img/charizard.png";
 
 export const Container = styled.div`
-  width: 100%;
-  height: 500px;
+  display: flex;
+  flex-direction: row;
+  height: 90vh;
+  margin: 0 0 0 24px;
 `;
+
+export const AlignLeftItens = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`;
+
+export const LogoCustom = styled.div`
+  background-image: url(${Logo});
+  width: 100%;
+  height: 100px;
+  background-repeat: no-repeat;
+  margin: 24px 0 0 0;
+`;
+
+export const Title = styled.div`
+  font-weight: bold;
+  font-size: 64px;
+  padding: 64px 0 0 0;
+`;
+
+export const Description = styled.div`
+  font-size: 20px;
+  width: 500px;
+  padding: 32px 0;
+`;
+
+export const ButtonCustom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 150px;
+  height: 40px;
+  background-color: #000000;
+  color: #ffffff;
+  border-radius: 20px;
+  cursor: pointer;
+`;
+
+export const AlignRightItens = styled.div`
+  width: 50%;
+`;
+
+export const SquareWallpaper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #b8c370;
+  transform: skew(-10deg, 0deg);
+  margin: 0 0px 0 70px;
+`;
+
+export const CharizardImg = styled.div`
+  background-image: url(${Charizard});
+  width: 70%;
+  height: 70%;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+// export const Container = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   width: 100%;
+//   height: 80vh;
+//   background-color: white;
+//   margin: 0 0 0 24px;
+// `;
+
+// export const SquareWallpaper = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   background-color: #b8c370;
+//   transform: skew(-10deg, 0deg);
+//   margin: 0 -146px 0 0;
+// `;
+
+// export const LogoCustom = styled.div`
+//   background-image: url(${Logo});
+//   width: 100%;
+//   background-repeat: no-repeat;
+//   margin: 24px 0 0 0;
+// `;
+
+// export const AlignHeader = styled.div`
+//   display: flex;
+// `;
+
+// export const Title = styled.div`
+//   font-weight: bold;
+//   font-size: 24px;
+// `;
